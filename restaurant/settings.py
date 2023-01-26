7e2daafe4e77ae1cffa52de27f58c01bbbd4e5e2
ShindanoSingi <shindanosingi@sekur.com>
2023-01-25 19:08:08 -0500

    c1

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*(dd==8sbq2=$9p2yfm9+o4y%d$wcx4e81(f_%=hl#_f4)$zg&"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
